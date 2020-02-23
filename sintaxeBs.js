7220a792af6addaa41519f8094bb334c8b65ea6d
Harrison Pepese <harrisonpepese@MacBook-Air-de-Harrison.local>
2020-02-23 17:08:55 -0300

    second commit

---
+++ sintaxeBs.js
@@ -0,0 +1,15 @@
+// Assim é um comentario de uma linha
+
+/**
+ * Assim é
+ * um
+ * comentário em 
+ * várias linhas
+ */
+
+ // declarando uma variável
+ var Variavel = 'var';
+ // Declarando uma constante
+ const Constante = 'const';
+ // Declarando outra variável
+ let variavel = 'anotherVar';
---
+++ tipos.js
@@ -0,0 +1,7 @@
+Boolean // O nosso conhecido true/false não precisa de apresentação
+String // O incrivel array de Caracteres e tbm não precisa.
+Number // Este é o type que engloba o int, float e double
+null //a palavra chave que indica que o valor de uma variavel é nulo
+undefined //a palavra chave que indica que o valor da variavel é indefinido
+Symbol // Um tipo de dado cuja as instâncias são únicas e imutáveis. eu nunca usei
+Object // Esse é o mais lindo e poderoso tipo do JS e vamos saber mais dele em outro post
